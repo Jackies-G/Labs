0babef9bfeb315c7d934074a1e283a19a4dd5e0d
Jun Qi Huang <jackyh9951@gmail.com>
2019-09-14 11:49:17 -0400

    Create Lab_Two.java

---
+++ Lab_Two.java
@@ -0,0 +1,34 @@
+public class Lab_Two {
+		public static void main(String[] args) {
+			System.out.println("                      O'Malley says, \" Prace\\ makes\\ perfect!\"                   ");
+			System.out.println("");
+			System.out.println("      Now, \\\\\" without further adeiu, a poem inside a frame:");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+			System.out.println("@");
+			System.out.println("@                        r-p-o-p-h-e-s-s-a-g-r");
+			System.out.println("@                       E. E. Cummings - 1894-1962");
+			System.out.println("@");
+			System.out.println("@");
+			System.out.println("@");
+			System.out.println("@                                          r-p-o-p-h-e-s-s-a-g-r");
+			System.out.println("@                               who");
+			System.out.println("@  a)s w(e loo)k");
+			System.out.println("@  upnowgath");
+			System.out.println("@                          PPEGORHRASS");
+			System.out.println("@                                                            eringint(o-");
+			System.out.println("@  aThe):1");
+			System.out.println("@                   eA");
+			System.out.println("@                         !p:");
+			System.out.println("@  S                                                                    a");
+			System.out.println("@                                    (r");
+			System.out.println("@ rIvInG                             .gRrEaPsPhOs)");
+			System.out.println("@                                                                      to");
+			System.out.println("@ rea(be)rran(com)gi(e)ngly");
+			System.out.println("@ ,grasshopper;");
+			System.out.println("@");
+			System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+	}
+}
